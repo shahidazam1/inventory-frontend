6c9474c13b100b29285bc6616966b290a901497f
shahid <shahidazam574@gmail.com>
2023-10-06 02:16:01 +0530

    home page

---
+++ src/pages/home.tsx
@@ -0,0 +1,32 @@
+import KeyboardArrowRightIcon from "@mui/icons-material/KeyboardArrowRight";
+import { Box, Button, Typography } from "@mui/material";
+import { useNavigate } from "react-router-dom";
+import { StyledCenter } from "styles";
+
+const HomePage = () => {
+  const navigate = useNavigate();
+
+  const handleClickStart = () => {
+    navigate("/inventory");
+  };
+  return (
+    <StyledCenter sx={{ height: "100vh" }}>
+      <Box textAlign="center">
+        <Typography variant="h5">
+          Welcome to Inventory Management App :)
+        </Typography>
+        <Button
+          sx={{ mt: 2 }}
+          variant="contained"
+          color="primary"
+          onClick={handleClickStart}
+          endIcon={<KeyboardArrowRightIcon />}
+        >
+          Let's Start
+        </Button>
+      </Box>
+    </StyledCenter>
+  );
+};
+
+export default HomePage;
